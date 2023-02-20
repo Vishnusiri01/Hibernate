7846fe3754fef85bb4f71d56fa8311c81099c02b
Vishnusiri01 <yallasirivishnu99@gmail.com>
2023-02-20 09:19:26 +0530

    first commit.

---
+++ springbootapplication/src/test/java/com/vishnu/demo1/SpringbootapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
